e49a59e843819dfeae7eb91b6f4a4bd2c54b1f3d
Kassem <systemry420@gmail.com>
2022-06-11 14:13:10 +0300

    run express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log('hell');
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/hello', (req, res) => {
+    res.send("hello world")
+})
+
+// GET  /api/tasks   - get all tasks
+// POST /api/task    - add new task
+// GET  /api/tasks/:id - get this task
+// PUT  /api/tasks/:id - edit this task
+// DELETE /api/tasks/:id - delete this task
+
+
+app.listen(port, () => {
+    console.log(`Server listening to port ${port}...`);
+})
