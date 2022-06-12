247026d7e2ac3a2a463e7c47499defa051d221d2
Kassem <systemry420@gmail.com>
2022-06-12 15:22:16 +0300

    delete and update task

--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -35,15 +35,33 @@ const getTask = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error })
     }
-    res.send('get task')
 }
 
-const editTask = (req, res) => {
-    res.send('edit task')
+const editTask = async (req, res) => {
+    try {
+        const { id: taskId } = req.params
+        const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+            new: true, runValidators: true
+        })
+
+        if(!task) {
+            return res.status(404).json({ message: `Task not found`})
+        }
+
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 }
 
-const deleteTask = (req, res) => {
-    res.send('delete task')
+const deleteTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params
+        const task = await Task.findOneAndDelete({ _id: taskID })
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 }
 
 module.exports = {
