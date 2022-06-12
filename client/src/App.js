0348d59d8206843e6dd1bdbbff9266af82d9bcd2
Kassem <systemry420@gmail.com>
2022-06-12 19:14:28 +0300

    addTask functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
+import { useState } from 'react'
 import Form from './components/Form';
 import List from './components/List';
 
 function App() {
+
+  const addTask = (data) => {
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <Form />
+      <h1>Task Manager</h1>
+      <Form addTask={addTask} />
       <List />
     </div>
   );
--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,14 +1,80 @@
-import { Button, TextField } from '@mui/material'
-import React from 'react'
+import React, { useState } from "react";
+import { Button, TextField } from "@mui/material";
 
-const Form = () => {
+const Form = ({ addTask }) => {
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+  });
+
+  const [error, setError] = useState({
+    name: false,
+    description: false
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!data.name) {
+        setError(prevState => ({
+            ...prevState,
+            name: true
+        }))
+        return;
+    }
+    if (!data.description) {
+        setError(prevState => ({
+            ...prevState,
+            description: true
+        }))
+        return;
+    }
+    setError({
+        name: false,
+        description: false    
+    })
+    setData({
+        name: "",
+        description: "",    
+    })
+    addTask(data)
+  }
+
+  const setValues = (e) => {
+    setData(prevState => {
+        return {
+            ...prevState,
+            [e.target.name]: e.target.value
+        }
+    })
+  }
+
   return (
-    <div className='form'>
-        <TextField placeholder='Enter title' />
-        <TextField placeholder='Enter description' />
-        <Button variant='contained'>Add Task</Button>
-    </div>
-  )
-}
+    <form className="form">
+      <TextField
+        variant="standard"
+        margin="normal"
+        value={data.name}
+        label="Enter name"
+        name='name'
+        required='true'
+        error={error.name}
+        onChange={setValues}
+      />
+      <TextField
+        variant="standard"
+        margin="normal"
+        value={data.description}
+        label="Enter description"
+        name='description'
+        required='true'
+        error={error.description}
+        onChange={setValues}
+      />
+      <Button onClick={handleSubmit} margin="normal" className="btn" variant="contained">
+        Add Task
+      </Button>
+    </form>
+  );
+};
 
-export default Form
+export default Form;
